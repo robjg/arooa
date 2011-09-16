01ef0edb100acca1fa8120842c415852982ed664
Rob Gordon <rob@rgordon.co.uk>
2011-09-16 16:46:35 +0100

    Add extra constructors to ConvertletException.

--- src/java/org/oddjob/arooa/convert/ConvertletException.java
+++ src/java/org/oddjob/arooa/convert/ConvertletException.java
@@ -3,9 +3,23 @@
  */
 package org.oddjob.arooa.convert;
 
+/**
+ * Thrown when a {@link Convertlet} can't convert something.
+ * 
+ * @author rob
+ *
+ */
 public class ConvertletException extends ArooaConversionException {
 	private static final long serialVersionUID = 20070202;
 
+	public ConvertletException(String message, Exception cause) {
+		super(message, cause);
+	}
+	
+	public ConvertletException(String message) {
+		super(message);
+	}
+	
 	public ConvertletException(Exception cause) {
 		super(cause);
 	}
