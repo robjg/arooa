20d078625ffa47280ab0bc38d3f9175d11aff5dc
Rob Gordon <rob@rgordon.co.uk>
2020-03-08 15:35:53 +0000

    Support Maven for Oddballs.

--- src/test/java/org/oddjob/OddjobSrc.java
+++ src/test/java/org/oddjob/OddjobSrc.java
@@ -19,24 +19,36 @@ public class OddjobSrc {
 	
 	private final File oddjobSrc;	
 
+	private final File oddjobApp;
+
 	public OddjobSrc() throws IOException {
-		String baseDir = System.getProperty("oddjob.src");
-		if (baseDir != null) {
-			oddjobSrc = new File(baseDir).getCanonicalFile();
+		String oddjobSrcFromProperty = System.getProperty("oddjob.src");
+		if (oddjobSrcFromProperty != null) {
+			oddjobSrc = new File(oddjobSrcFromProperty).getCanonicalFile();
 			logger.info("oddjob.src=" + oddjobSrc.toString());
 		}
 		else {
 			File pwd = new File(".").getCanonicalFile();
 			if ("oddjob".equals(pwd.getName())) {
-				logger.info("The appears to be the oddjob project.");
+				logger.info("This appears to be the oddjob project.");
 				oddjobSrc = new File(".");
 			}
 			else {
-				logger.info("Guess oddjob.src to be a parrallel directory.");
+				logger.info("Guess oddjob.src to be a parallel directory.");
 				oddjobSrc = new OurDirs().relative("../oddjob").getCanonicalFile();
 			}
 		}
-		
+
+		String appDirFromProperty = System.getProperty("oddjob.app");
+		if (appDirFromProperty != null) {
+			oddjobApp = new File(appDirFromProperty).getCanonicalFile();
+			logger.info("oddjob.app=" + oddjobApp.toString());
+		}
+		else {
+			logger.info("Guess oddjob.app to be a ../run-oddjob/target/app directory.");
+			oddjobApp = new OurDirs().relative("../run-oddjob/target/app").getCanonicalFile();
+		}
+
 		if (!oddjobSrc.exists()) {
 			throw new FileNotFoundException(oddjobSrc + " does not exist.");
 		}
@@ -45,5 +57,11 @@ public class OddjobSrc {
 	public File oddjobSrcBase() {
 		return oddjobSrc;
 	}
-	
+
+	public File oddjobApp() throws FileNotFoundException {
+		if (!oddjobApp.exists()) {
+			throw new FileNotFoundException("Oddjob App " + oddjobApp + " does not exist.");
+		}
+		return oddjobApp;
+	}
 }
