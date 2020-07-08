be1d28d5bcc8519523a7da1aa70fe7bbe2cc72e2
Rob Gordon <rob@rgordon.co.uk>
2020-07-08 13:29:13 +0100

    Add Web Oddjob Remote.

---
+++ src/main/java/org/oddjob/arooa/utils/Pair.java
@@ -0,0 +1,55 @@
+package org.oddjob.arooa.utils;
+
+import java.util.Objects;
+
+/**
+ * A simple pair type. Allows nulls.
+ *
+ * @param <L> Left type.
+ * @param <R> Right type.
+ */
+public class Pair<L, R> {
+
+    private final L left;
+
+    private final R right;
+
+    private Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) &&
+                Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
