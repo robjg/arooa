c0fbb15aaa966e20e74ef526be4d27b9b0f41786
Rob Gordon <rob@rgordon.co.uk>
2022-08-08 14:15:19 +0100

    Fix problems with destroying nodes caused by introducing the RemoteConnection abstraction.

--- src/main/java/org/oddjob/arooa/utils/ListenerSupportBase.java
+++ src/main/java/org/oddjob/arooa/utils/ListenerSupportBase.java
@@ -9,70 +9,81 @@ import java.util.List;
  * This is useful when the events are being notified in a chain and
  * there is no need to subscribe when nothing is listening. For instance
  * in the Oddjob JMX handlers.
- * 
- * @author rob
  *
  * @param <T> The type of the listener.
+ * @author rob
  */
 public class ListenerSupportBase<T> {
 
-	private final List<T> listeners =
-		new ArrayList<T>();
-	
-	private Runnable onFirst;
-	
-	private Runnable onEmpty;
-	
-	protected List<T> copy() {
-		synchronized (listeners) {
-			return new ArrayList<T>(listeners);
-		}
-		
-	}
-	
-	protected void addListener(T listener) {
-		synchronized (listeners) {
-			if (listeners.isEmpty() && onFirst != null) {
-				onFirst.run();
-			}
-			listeners.add(listener);
-		}
-	}
-	
-	protected void removeListener(T listener) {
-		synchronized (listeners) {
-			listeners.remove(listener);
-			if (listeners.isEmpty() && onEmpty != null) {
-				onEmpty.run();
-			}
-		}
-	}
-	
-	public Runnable getOnFirst() {
-		return onFirst;
-	}
+    private final List<T> listeners =
+            new ArrayList<>();
 
-	/**
-	 * Set an action to run when the first listener
-	 * is added.
-	 * 
-	 * @param onFirst
-	 */
-	public void setOnFirst(Runnable onFirst) {
-		this.onFirst = onFirst;
-	}
+    private Runnable onFirst;
 
-	public Runnable getOnEmpty() {
-		return onEmpty;
-	}
+    private Runnable onEmpty;
 
-	/**
-	 * Set an action to be run when the last listener has
-	 * been removed.
-	 * 
-	 * @param onEmpty
-	 */
-	public void setOnEmpty(Runnable onEmpty) {
-		this.onEmpty = onEmpty;
-	}
+    protected List<T> copy() {
+        synchronized (listeners) {
+            return new ArrayList<>(listeners);
+        }
+
+    }
+
+    protected void addListener(T listener) {
+        synchronized (listeners) {
+            if (listeners.isEmpty() && onFirst != null) {
+                onFirst.run();
+            }
+            listeners.add(listener);
+        }
+    }
+
+    protected void removeListener(T listener) {
+        synchronized (listeners) {
+            listeners.remove(listener);
+            if (listeners.isEmpty() && onEmpty != null) {
+                onEmpty.run();
+            }
+        }
+    }
+
+    public Runnable getOnFirst() {
+        return onFirst;
+    }
+
+    /**
+     * Set an action to run when the first listener
+     * is added.
+     *
+     * @param onFirst
+     */
+    public void setOnFirst(Runnable onFirst) {
+        this.onFirst = onFirst;
+    }
+
+    public Runnable getOnEmpty() {
+        return onEmpty;
+    }
+
+    /**
+     * Set an action to be run when the last listener has
+     * been removed.
+     *
+     * @param onEmpty
+     */
+    public void setOnEmpty(Runnable onEmpty) {
+        this.onEmpty = onEmpty;
+    }
+
+    /**
+     * Remove listeners on run {@link #onEmpty}.
+     */
+    public void destroy() {
+        synchronized (listeners) {
+            if (!listeners.isEmpty()) {
+                listeners.clear();
+                onEmpty.run();
+            }
+        }
+    }
 }
