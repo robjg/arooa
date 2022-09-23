e40017ec6588001a9e01dd01fb35e2ac22a6fccb
Rob Gordon <rob@rgordon.co.uk>
2022-09-23 06:46:04 +0100

    Fix saving screen position in Explorer. Rework Async support.

--- src/main/java/org/oddjob/arooa/design/view/ScreenPresence.java
+++ src/main/java/org/oddjob/arooa/design/view/ScreenPresence.java
@@ -1,35 +1,37 @@
 package org.oddjob.arooa.design.view;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.io.IOException;
 import java.io.Serializable;
 
 /**
  * Represent location and size information for a screen
- * component.
+ * component. Mutable so that the position can be updated and saved.
  * 
  * @author rob
  *
  */
 public class ScreenPresence implements Serializable {
-	private static final long serialVersionUID = 2011042600L;
+	private static final long serialVersionUID = 2022092100L;
+
+	private static final int PREFERRED_HEIGHT = 600;
+
+	private static final int PREFERRED_WIDTH = 800;
+
+	private volatile Point location;
 	
-	private final Point location;
-	
-	private final Dimension size;
+	private volatile Dimension size;
 
 	public ScreenPresence(Point position, Dimension size) {
 		this.location = position;
 		this.size = size;
 	}
-	
+
 	public ScreenPresence(Component component) {
 		this.location = component.getLocation();
 		this.size = component.getSize();
 	}
-	
+
 	/**
 	 * Fit a component to the location and size of this 
 	 * screen presence.
@@ -75,7 +77,12 @@ public class ScreenPresence implements Serializable {
 		
 		return new Point(x, y);
 	}
-	
+
+	public void adjustTo(Component component) {
+		this.location = component.getLocation();
+		this.size = component.getSize();
+	}
+
 	/**
 	 * Create ScreenPresence a factor the size of this. The location will 
 	 * be adjusted so that it is offset equally in both x and y direction
@@ -96,11 +103,42 @@ public class ScreenPresence implements Serializable {
 				size);
 	}
 	
-	public static ScreenPresence wholeScreen() {
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		return new ScreenPresence(new Point(0, 0), dim);
+	public static ScreenPresence defaultSize(Dimension screenSize) {
+
+		if (screenSize.getWidth() <= 800 || screenSize.getHeight() <= 600) {
+			return new ScreenPresence(new Point(0, 0), screenSize);
+		}
+		else {
+			int x = (int) (screenSize.getWidth() - PREFERRED_WIDTH) / 2;
+			int y = (int) (screenSize.getHeight() - PREFERRED_HEIGHT) / 2;
+
+			return new ScreenPresence(new Point(x, y), new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT));
+		}
 	}
-	
+
+	public static ScreenPresence defaultSize() {
+
+		return defaultSize(Toolkit.getDefaultToolkit().getScreenSize());
+	}
+
+	/**
+	 * Custom de-serialisation.
+	 */
+	private Object readResolve()
+			throws IOException, ClassNotFoundException {
+
+		Dimension wholeScreen = Toolkit.getDefaultToolkit().getScreenSize();
+
+		if (location.getX() > (wholeScreen.getWidth() - 100) ||
+			location.getY() > (wholeScreen.getHeight() - 100)) {
+
+			return defaultSize(wholeScreen);
+		}
+		else {
+			return this;
+		}
+	}
+
 	@Override
 	public String toString() {
 		return getClass().getSimpleName() + ": location=(" + location.getX() + 
