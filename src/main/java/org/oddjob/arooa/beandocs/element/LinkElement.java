cde73a232af1dde80f62faab1868bc602441437d
Rob Gordon <rob@rgordon.co.uk>
2024-03-21 07:14:48 +0000

    Generalise doc link processing.

--- src/main/java/org/oddjob/arooa/beandocs/element/LinkElement.java
+++ src/main/java/org/oddjob/arooa/beandocs/element/LinkElement.java
@@ -1,16 +1,25 @@
 package org.oddjob.arooa.beandocs.element;
 
 /**
- * A link to either another reference item or java doc.
+ * A link to either another reference item or java doc. Processed
+ * from the {@link LinkElement#setPropertyName(String) A link with a property}
  */
 public class LinkElement implements BeanDocElement {
 
+    /** The label if any after the actual link specification. In the above
+     * it would be 'A link with a property.' */
     private String label;
 
+    /** The link ref as it appears in the tag. In the above it would be
+     * @{code LinkElement#setPropertyName(String)} */
     private String signature;
 
+    /** The qualified type of the Link. In the above it would be
+     * {@code org.oddjob.arooa.beandocs.element.LinkElement}. */
     private String qualifiedType;
 
+    /** The derived property name if it's possible from the field or getter or setter.
+     * In the above example it would {@code propertyName}. */
     private String propertyName;
 
     public String getLabel() {
